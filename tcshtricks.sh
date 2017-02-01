da2f621f569f0df2c3dea0272ed6fb97ecb9b135
TrackDR <jeremydjackson@gmail.com>
2017-02-01 11:36:35 -0500

    Update tcshtricks.sh

--- tcshtricks.sh
+++ tcshtricks.sh
@@ -10,16 +10,33 @@ end
 set fname="/usr/local/mytest.txt"
 
 # Remove a trailing pathname component, leaving the head.
+# /usr/local
 echo $fname:h 
+
 # Remove all leading pathname components, leaving the tail.
+# mytest.txt
 echo $fname:t 
+
 # Remove all but the extension.
+# txt
 echo $fname:e 
+
 # Remove a filename extension `.xxx', leaving the root name.
+# /usr/local/mytest
 echo $fname:r 
+
+# Remove a filename extension `.xxx', leaving the root name and then remove all leading pathname components, leaving the tail.
+# mytest
+echo $fname:r :t
+
 # Uppercase the first lowercase letter.
+# /Usr/local/mytest
 echo $fname:u 
+
 # Lowercase the first uppercase letter.
+#  /usr/local/mytest.txt
 echo $fname:l 
+
 # l is simply a string like r, not a regular expression. 
+#
 echo $fname:s/l/r/ Substitute l for r. 
